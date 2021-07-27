b6ec413bd3583c738a1eaf7bda319aca0030e4c6
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-07-27 18:39:21 +0900

    07.27. study

---
+++ code/Jeonghwan-code/sol04.js
@@ -0,0 +1,11 @@
+let a = 1;
+console.log(typeof a); // number
+
+a = 2.22;
+console.log(typeof a); // number
+
+a = "p";
+console.log(typeof a); // string
+
+a = [1, 2, 3];
+console.log(typeof a); // object
---
+++ code/Jeonghwan-code/sol07.js
@@ -0,0 +1,5 @@
+const age;
+const Age;
+// const let; 'let'은 예약어이므로 변수명으로 사용 불가능
+const _age;
+// const 1age; 변수명은 숫자로 시작하여 사용 불가능
