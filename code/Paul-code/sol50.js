037112b8c7714a231eb7f65d36fd0a25a1a6c008
kbu715 <kbu715@gmail.com>
2021-08-01 12:53:03 +0900

    0801 commit

---
+++ code/Paul-code/sol50.js
@@ -0,0 +1,22 @@
+// 버블정렬
+
+function bubble(arr) {
+    let result = arr.slice(); 
+  
+    for (let i = 0; i < result.length - 1; i++) {
+      for (let j = result.length; j > i; j--) {
+        if (result[j] < result[j - 1]) {
+           let temp = result[j];
+           result[j] = result[j - 1];
+           result[j - 1] = temp;
+        }
+      }
+    }
+    return result;
+  }
+  
+  const items = prompt('입력해주세요.').split(' ').map((n) => {
+    return parseInt(n, 10);
+  });
+  
+  console.log(bubble(items));
