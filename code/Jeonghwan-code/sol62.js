cb96b5a4c49a70e35800c47fa3da72e7cddf6d57
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-08-05 23:18:24 +0900

    08.05. study

---
+++ code/Jeonghwan-code/sol62.js
@@ -0,0 +1,10 @@
+const user_s = "aacdddddddddfffffffffgghhh";
+let result_s = "";
+
+console.log(
+  `${user_s.match(/a/g).length}${Number(user_s.match(/b/g))}${
+    user_s.match(/c/g).length
+  }${user_s.match(/d/g).length}${Number(user_s.match(/e/g))}${
+    user_s.match(/f/g).length
+  }${user_s.match(/g/g).length}${user_s.match(/h/g).length}`
+);
